62a511e5569f18594469e8daa75cccc1b7307ffd
Rafael <rafael.gcaldas01@gmail.com>
2022-02-11 00:23:23 -0300

    Managing rendering

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -46,9 +46,13 @@ export default function Home(): JSX.Element {
     })
   }, [data]);
 
-  // TODO RENDER LOADING SCREEN
+  if (isLoading) {
+    return <Loading />
+  }
 
-  // TODO RENDER ERROR SCREEN
+  if (isError) {
+    return <Error />
+  }
 
   return (
     <>
@@ -56,7 +60,7 @@ export default function Home(): JSX.Element {
 
       <Box maxW={1120} px={20} mx="auto" my={20}>
         <CardList cards={formattedData} />
-        {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
+        <Button>Carregar mais</Button>
       </Box>
     </>
   );
